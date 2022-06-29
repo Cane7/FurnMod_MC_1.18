6b47f7d0c532d849c8b36eeecb8984c9428759ea
Nace <nace.malezic@gmail.com>
2022-06-29 21:14:17 +0200

    Initial commit

---
+++ src/main/java/com/cane7/furnmod/item/ModCreativeModeTab.java
@@ -0,0 +1,15 @@
+package com.cane7.furnmod.item;
+
+import com.cane7.furnmod.block.ModBlocks;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModeTab {
+
+    public static final CreativeModeTab FURNMOD_TAB = new CreativeModeTab("furnmodtab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModBlocks.CRACKED_BLOCK.get());
+        }
+    };
+}
