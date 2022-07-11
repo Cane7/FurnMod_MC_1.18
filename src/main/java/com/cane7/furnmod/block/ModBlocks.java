2afffe96d560bf8dae830285c9a188268440cd8a
Nace <nace.malezic@gmail.com>
2022-07-11 20:16:59 +0200

    Added Furn Logs (log, wood, stripped log, stripped wood) and Planks. No textures.

--- src/main/java/com/cane7/furnmod/block/ModBlocks.java
+++ src/main/java/com/cane7/furnmod/block/ModBlocks.java
@@ -1,19 +1,25 @@
 package com.cane7.furnmod.block;
 
 import com.cane7.furnmod.FurnMod;
+import com.cane7.furnmod.block.custom.ModFlammableRotatedPillarBlock;
 import com.cane7.furnmod.item.ModCreativeModeTab;
 import com.cane7.furnmod.item.ModItems;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ModBlocks {
@@ -22,9 +28,37 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, FurnMod.MOD_ID);
 
 
-    public static final RegistryObject<Block> CRACKED_BLOCK = registerBlock("cracked_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.FURNMOD_TAB);
+    public static final RegistryObject<Block> FURN_LOG = registerBlock("furn_log",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)),
+            ModCreativeModeTab.FURNMOD_TAB);
+    public static final RegistryObject<Block> FURN_WOOD = registerBlock("furn_wood",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)),
+            ModCreativeModeTab.FURNMOD_TAB);
+    public static final RegistryObject<Block> STRIPPED_FURN_LOG = registerBlock("stripped_furn_log",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG)),
+            ModCreativeModeTab.FURNMOD_TAB);
+    public static final RegistryObject<Block> STRIPPED_FURN_WOOD = registerBlock("stripped_furn_wood",
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_WOOD)),
+            ModCreativeModeTab.FURNMOD_TAB);
+
+
+    public static final RegistryObject<Block> FURN_PLANKS = registerBlock("furn_planks",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)) {
+                @Override
+                public boolean isFlammable(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+                    return true;
+                }
+
+                @Override
+                public int getFlammability(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+                    return 20;
+                }
+
+                @Override
+                public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
+                    return 5;
+                }
+            }, ModCreativeModeTab.FURNMOD_TAB);
 
 
 
